cafc0e2addb4fe049c9695dedda2f03f71777f3f
boxp <tiyotiyouda@gmail.com>
2016-04-11 10:42:13 +0900

    Add setup.sh

---
+++ setup.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+ln -s ~/.xmonad ~/dotfiles/.xmonad
+ln -s ~/.Xmodmap ~/dotfiles/.Xmodmap
+ln -s ~/.Xresources ~/dotfiles/Xresources
+ln -s ~/.vimrc ~/dotfiles/.vimrc
+ln -s ~/.xmobarrc ~/dotfiles/.xmobarrc
+ln -s ~/.xsession ~/dotfiles/.xsession
+ln -s ~/.zprofile ~/dotfiles/.zprofile
+ln -s ~/.zshrc ~/dotfiles/.zshrc
