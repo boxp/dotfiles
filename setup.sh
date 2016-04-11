92fecbfa393336f14503aeb3d435f355c49bceb7
boxp <tiyotiyouda@gmail.com>
2016-04-11 10:43:44 +0900

    Fix symbolic link path

--- setup.sh
+++ setup.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-ln -s ~/.xmonad ~/dotfiles/.xmonad
-ln -s ~/.Xmodmap ~/dotfiles/.Xmodmap
-ln -s ~/.Xresources ~/dotfiles/Xresources
-ln -s ~/.vimrc ~/dotfiles/.vimrc
-ln -s ~/.xmobarrc ~/dotfiles/.xmobarrc
-ln -s ~/.xsession ~/dotfiles/.xsession
-ln -s ~/.zprofile ~/dotfiles/.zprofile
-ln -s ~/.zshrc ~/dotfiles/.zshrc
+ln -s ~/dotfiles/.xmonad ~/.xmonad
+ln -s ~/dotfiles/.Xmodmap ~/.Xmodmap
+ln -s ~/dotfiles/.Xresources ~/Xresources
+ln -s ~/dotfiles/.vimrc ~/.vimrc
+ln -s ~/dotfiles/.xmobarrc ~/.xmobarrc
+ln -s ~/dotfiles/.xsession ~/.xsession
+ln -s ~/dotfiles/.zprofile ~/.zprofile
+ln -s ~/dotfiles/.zshrc ~/.zshrc
